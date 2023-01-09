56b4ee9db0e17dd962de91c3b5bcfa710c9a0b93
Lin Moskovitch <lin.moskovitch@sap.com>
2023-01-09 17:35:29 +0200

    app is running

--- Notify/Notify/Notify.Android/MainActivity.cs
+++ Notify/Notify/Notify.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Notify.Droid
 {
-    [Activity(Label = "Formula1", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Notify", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
