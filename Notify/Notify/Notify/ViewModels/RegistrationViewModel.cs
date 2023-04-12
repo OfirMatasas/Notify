9181f2c8d74818ab3a082ccc67e3d8e5f7a82eec
Lin Moskovitch <lin.moskovitch@sap.com>
2023-04-12 23:18:34 +0300

    added telephone validation

--- Notify/Notify/Notify/ViewModels/RegistrationViewModel.cs
+++ Notify/Notify/Notify/ViewModels/RegistrationViewModel.cs
@@ -1,11 +1,48 @@
+using System.ComponentModel;
+using System.Diagnostics;
+using Xamarin.Forms;
+using System.Text.RegularExpressions;
+
+
 namespace Notify.ViewModels
 {
     public class RegistrationPageViewModel
     {
-        public object UserName { get; }
-        public object Email { get; }
-        public object Password { get; }
-        public object ConfirmPassword { get; }
-        public object RegisterCommand { get; }
+        public string Name { get; }
+        public string UserName { get; }
+        public string Password { get; }
+        public string ConfirmPassword { get; }
+        public string RegisterCommand { get; }
+        
+        private string m_Telephone;
+        public string Telephone
+        {
+            get => m_Telephone;
+            set
+            {
+                m_Telephone = value;
+                OnPropertyChanged(nameof(Telephone));
+                ValidateTelephone();
+            }
+        }
+        
+        private void ValidateTelephone()
+        {
+            bool isValid = !string.IsNullOrEmpty(Telephone) && Regex.IsMatch(Telephone, @"^\d{10}$");
+
+            if (!isValid)
+            {
+                Debug.WriteLine("Please enter a valid 10-digit telephone number.");
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
     }
 }
