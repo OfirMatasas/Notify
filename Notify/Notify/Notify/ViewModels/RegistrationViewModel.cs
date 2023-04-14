ab32b6d7aca7f085f7d7cbfae37723691417256c
Lin Moskovitch <lin.moskovitch@sap.com>
2023-04-14 17:29:21 +0300

    working on input validations

--- Notify/Notify/Notify/ViewModels/RegistrationViewModel.cs
+++ Notify/Notify/Notify/ViewModels/RegistrationViewModel.cs
@@ -1,20 +1,26 @@
 using System.ComponentModel;
 using System.Diagnostics;
-using Xamarin.Forms;
+using System.IO;
 using System.Text.RegularExpressions;
-
+using Newtonsoft.Json;
+using Xamarin.Forms;
 
 namespace Notify.ViewModels
 {
-    public class RegistrationPageViewModel
+    public class RegistrationPageViewModel : INotifyPropertyChanged
     {
-        public string Name { get; }
-        public string UserName { get; }
-        public string Password { get; }
-        public string ConfirmPassword { get; }
-        public string RegisterCommand { get; }
-        
+        private object m_SignUpCommand;
         private string m_Telephone;
+        private bool m_IsFormValid;
+
+        public string Name { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
         public string Telephone
         {
             get => m_Telephone;
@@ -25,24 +31,90 @@ namespace Notify.ViewModels
                 ValidateTelephone();
             }
         }
+
+        public bool IsFormValid
+        {
+            get => m_IsFormValid;
+            set
+            {
+                m_IsFormValid = value;
+                OnPropertyChanged(nameof(IsFormValid));
+            }
+        }
+
+        private void ValidateName()
+        {
+            bool isValid = !string.IsNullOrEmpty(Name) && Regex.IsMatch(Name, @"^[a-zA-Z]+$");
+
+            if (!isValid)
+            {
+                DisplayError("Please enter a valid name consisting only of letters.");
+            }
+        }
+
+        private void ValidatePassword()
+        {
+            bool isValid = !string.IsNullOrEmpty(Password) && Password == ConfirmPassword;
+
+            if (!isValid)
+            {
+                DisplayError("Please enter matching passwords.");
+            }
+        }
         
         private void ValidateTelephone()
         {
             bool isValid = !string.IsNullOrEmpty(Telephone) && Regex.IsMatch(Telephone, @"^\d{10}$");
-
+            if (isValid && !Telephone.StartsWith("05"))
+            {
+                isValid = false;
+            }
+        
             if (!isValid)
             {
-                Debug.WriteLine("Please enter a valid 10-digit telephone number.");
+                DisplayError("Please enter a valid 10-digit telephone number starting with '05'.");
             }
         }
-        
+
+        private void DisplayError(string message)
+        {
+            Application.Current.MainPage.DisplayAlert("Error", message, "OK");
+        }
+
+        private void OnSignUpClicked()
+        {
+            ValidateName();
+            ValidatePassword();
+            ValidateTelephone();
+
+            if (!string.IsNullOrEmpty(Name) && !string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(Password) && !string.IsNullOrEmpty(ConfirmPassword) && !string.IsNullOrEmpty(Telephone) && IsFormValid)
+            {
+                var registrationData = new
+                {
+                    Name,
+                    UserName,
+                    Password,
+                    Telephone
+                };
+
+                // Convert the registration data to JSON and write it to a file
+                string jsonData = JsonConvert.SerializeObject(registrationData);
+                string fileName = "registration_data.json";
+                File.WriteAllText(fileName, jsonData);
+
+                Application.Current.MainPage.DisplayAlert("Success", "You have successfully signed up!", "OK");
+            }
+            else
+            {
+                DisplayError("Please fill in all required fields.");
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
-
     }
 }
