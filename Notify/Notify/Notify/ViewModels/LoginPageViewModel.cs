190ee30df448dd96b75ce32041226acec30e1a39
Lin Moskovitch <lin.moskovitch@sap.com>
2023-04-15 18:56:01 +0300

    store user credentials on device cache

--- Notify/Notify/Notify/ViewModels/LoginPageViewModel.cs
+++ Notify/Notify/Notify/ViewModels/LoginPageViewModel.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Notify.Helpers;
 using Notify.Services.Location;
@@ -8,17 +10,16 @@ using Xamarin.Forms;
 
 namespace Notify.ViewModels
 {
-    public class LoginPageViewModel : BaseViewModel
+    public class LoginPageViewModel : BaseViewModel, INotifyPropertyChanged
     {
-        public Command LogInCommand { get; set; }
-        public Command SignUpCommand { get; set; }
-        
-        private LocationService locationService;
-        
         private string m_UserName;
         private string m_Password;
-        private Task Init { get; }
-        
+        private bool m_RememberMe;
+        private LocationService locationService;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Command LogInCommand { get; set; }
+        public Command SignUpCommand { get; set; }
+
         public string UserName
         {
             get => m_UserName;
@@ -30,15 +31,27 @@ namespace Notify.ViewModels
             get => m_Password;
             set => SetProperty(ref m_Password, value);
         }
-        
+
+        public bool RememberMe
+        {
+            get => m_RememberMe;
+            set
+            {
+                if (m_RememberMe != value)
+                {
+                    m_RememberMe = value;
+                    OnPropertyChanged(nameof(RememberMe));
+                }
+            }
+        }
+
         public LoginPageViewModel()
         {
             LogInCommand = new Command(onLoginClicked);
             SignUpCommand = new Command(onSignUpClicked);
-
-            Init = initialize();
+            initialize();
         }
-        
+
         private async Task initialize()
         {
             locationService = new LocationService();
@@ -52,21 +65,39 @@ namespace Notify.ViewModels
             {
                 await Shell.Current.GoToAsync("///welcome");
             }
-            
+
             locationService.SubscribeToLocationMessaging();
         }
-        
+
         private async void onLoginClicked()
         {
             bool debugAutoLogin = true;
             IsBusy = true;
-            
-            if (!debugAutoLogin)
+
+            if (debugAutoLogin)
             {
                 try
                 {
-                    if (m_UserName.Equals(Constants.USERNAME) && Password.Equals(Constants.PASSWORD))
+                    bool areCredentialsValid =
+                        m_UserName.Equals(Constants.USERNAME) && Password.Equals(Constants.PASSWORD);
+
+                    if (areCredentialsValid)
                     {
+                        if (RememberMe)
+                        {
+                            if (!Preferences.ContainsKey("NotifyUserName"))
+                            {
+                                Preferences.Set("NotifyUserName", Constants.USERNAME);
+                            }
+
+                            if (!Preferences.ContainsKey("NotifyPassword"))
+                            {
+                                Preferences.Set("NotifyPassword", Constants.PASSWORD);
+                            }
+
+                            Debug.WriteLine("User credentials saved in preferences");
+                        }
+
                         await locationService.ManageLocationTracking();
                         await Shell.Current.GoToAsync("///main");
                     }
@@ -74,7 +105,6 @@ namespace Notify.ViewModels
                     {
                         await App.Current.MainPage.DisplayAlert("Error", "Invalid credentials", "OK");
                     }
-
                 }
                 catch (Exception)
                 {
@@ -93,6 +123,11 @@ namespace Notify.ViewModels
         private async void onSignUpClicked()
         {
             await Shell.Current.GoToAsync("///register");
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
