7bb26f925279ad95b7003090c0a11114aff43557
Lin Moskovitch <82546057+LinMoskovitch@users.noreply.github.com>
2023-04-19 22:35:08 +0300

    feat: make login page UI more responsive (#45)

--- Notify/Notify/Notify/ViewModels/RegistrationViewModel.cs
+++ Notify/Notify/Notify/ViewModels/RegistrationViewModel.cs
@@ -1,6 +1,8 @@
+using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
+using Notify.Helpers;
 using Xamarin.Forms;
 
 namespace Notify.ViewModels
@@ -30,6 +32,16 @@ namespace Notify.ViewModels
         
         public event PropertyChangedEventHandler PropertyChanged;
         
+        public object NameBorderColor { get; set; }
+        
+        public object UserNameBorderColor { get; set; }
+        
+        public object TelephoneBorderColor { get; set; }
+        
+        public object PasswordBorderColor { get; set; }
+        
+        public object ConfirmPasswordBorderColor { get; set; }
+        
         public string Telephone
         {
             get => m_Telephone;
@@ -54,11 +66,16 @@ namespace Notify.ViewModels
         {
             bool isValid = !string.IsNullOrEmpty(Name) && Regex.IsMatch(Name, @"^[a-zA-Z ]+$");
 
-            if (!isValid)
+            if (isValid)
             {
+                NameBorderColor = Constants.VALID_COLOR;
+            }
+            else
+            {
+                NameBorderColor = Constants.INVALID_COLOR;
                 displayError("Please enter a valid name consisting only of letters.");
             }
-
+            
             return isValid;
         }
 
@@ -68,8 +85,14 @@ namespace Notify.ViewModels
                            Regex.IsMatch(Password, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$") &&
                            Password == ConfirmPassword;
 
-            if (!isValid)
+            if (isValid)
             {
+                PasswordBorderColor = ConfirmPasswordBorderColor = Constants.VALID_COLOR;
+            }
+            else
+            {
+                PasswordBorderColor = ConfirmPasswordBorderColor = Constants.INVALID_COLOR;
+
                 if (string.IsNullOrEmpty(Password))
                 {
                     displayError("Please enter a password.");
@@ -83,35 +106,54 @@ namespace Notify.ViewModels
                     displayError("Please enter a password containing at least 8 characters, with at least one uppercase letter, one lowercase letter, one number, and one special character.");
                 }
             }
-
+    
             return isValid;
         }
+
+        
+        private bool validateUserName()
+        {
+            bool isValid = !string.IsNullOrEmpty(UserName) && Regex.IsMatch(UserName, @"^[a-zA-Z0-9]+$");
+    
+            if (isValid)
+            {
+                UserNameBorderColor = Constants.VALID_COLOR;
+            }
+            else
+            {
+                UserNameBorderColor = Constants.INVALID_COLOR;
+                displayError("Please enter a valid username consisting only of letters and numbers.");
+            }
+    
+            return isValid;
+        }
+
         
         private bool validateTelephone()
         {
+            bool isValid = false;
+
             if (!string.IsNullOrEmpty(Telephone))
             {
-                if (Telephone.StartsWith("0"))
+                if (Regex.IsMatch(Telephone, @"^05\d{8}$"))
                 {
-                    if (!Telephone.StartsWith("05"))
-                    {
-                        displayError("Please enter a valid 10-digit telephone number starting with '05'.");
-                        return false;
-                    }
+                    isValid = true;
                 }
-
-                bool isValid = Regex.IsMatch(Telephone, @"^\d{10}$");
-
-                if (!isValid)
+                else
                 {
                     displayError("Please enter a valid 10-digit telephone number starting with '05'.");
                 }
-
-                return isValid;
+            }
+            else
+            {
+                displayError("Please enter a telephone number.");
             }
 
-            return true;
+            TelephoneBorderColor = isValid ? Constants.VALID_COLOR : Constants.INVALID_COLOR;
+
+            return isValid;
         }
+
         
         private void displayError(string message)
         {
@@ -126,9 +168,14 @@ namespace Notify.ViewModels
                 return;
             }
 
-            if (validateName() && validatePassword() && validateTelephone())
+            bool isNameValid = validateName();
+            bool isUserNameValid = validateUserName();
+            bool isPasswordValid = validatePassword();
+            bool isTelephoneValid = validateTelephone();
+
+            if (isNameValid && isUserNameValid && isPasswordValid && isTelephoneValid)
             {
-                Debug.WriteLine($"You have successfully signed up!\nName: {Name}\nUserName: {UserName}\nPassword: {Password}\nTelephone: {Telephone}");
+                Debug.WriteLine($"You have successfully signed up!{Environment.NewLine}Name: {Name}{Environment.NewLine}UserName: {UserName}{Environment.NewLine}Password: {Password}{Environment.NewLine}Telephone: {Telephone}");
                 await Application.Current.MainPage.DisplayAlert("Success", "You have successfully signed up!", "OK");
                 await Shell.Current.GoToAsync("///login");
             }
