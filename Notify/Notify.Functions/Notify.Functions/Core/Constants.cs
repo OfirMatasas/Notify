4adc9ae28938e4907fc59c6705b54f0f7e0e88e3
ofirmatasas <ofir.matasas@cyberark.com>
2023-04-26 22:10:12 +0300

    Fixed collections naming

--- Notify/Notify.Functions/Notify.Functions/Core/Constants.cs
+++ Notify/Notify.Functions/Notify.Functions/Core/Constants.cs
@@ -3,7 +3,11 @@ namespace Notify.Functions.Core
     public static class Constants
     {
         public static readonly string DATABASE_NOTIFY_MTA = "Notify_MTA";
-        public static readonly string COLLECTION_DESTINATIONS = "Destination";
-        public static readonly string COLLECTION_NOTIFICATIONS = "Notifications";
+        public static readonly string COLLECTION_DESTINATION = "Destination";
+        public static readonly string COLLECTION_NOTIFICATION = "Notification";
+        public static readonly string COLLECTION_USER = "User";
+        public static readonly string COLLECTION_FRIEND = "Friend";
+        public static readonly string COLLECTION_FRIEND_REQUEST = "Friend_Request";
+        public static readonly string COLLECTION_GROUP = "Group";
     }
 }
