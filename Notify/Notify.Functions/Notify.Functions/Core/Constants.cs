17f39b4690089a7ea487f205817eb2a79a6bf5a7
ofirmatasas <ofir.matasas@cyberark.com>
2023-04-28 13:19:28 +0300

    Fixed database connection string error

--- Notify/Notify.Functions/Notify.Functions/Core/Constants.cs
+++ Notify/Notify.Functions/Notify.Functions/Core/Constants.cs
@@ -3,6 +3,7 @@ namespace Notify.Functions.Core
     public static class Constants
     {
         public static readonly string DATABASE_NOTIFY_MTA = "Notify_MTA";
+        public static readonly string CONNECTION_STRING = @"mongodb://notify-mta-database:7TybNmpnXUblzkln3VjZqlX0t6MT9QekAoKnImhzLM0zBr5uphSVCjwzgBX6XjNRzVcnaSygAwVRACDbApzIsg==@notify-mta-database.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@notify-mta-database@";
         public static readonly string COLLECTION_DESTINATION = "Destination";
         public static readonly string COLLECTION_NOTIFICATION = "Notification";
         public static readonly string COLLECTION_USER = "User";
