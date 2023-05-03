dfbbc6c56f9a6cacb535b07bfe1e33f72a1f2d0b
ofirmatasas <ofir.matasas@cyberark.com>
2023-05-03 17:46:51 +0300

    Showing friends on page

--- Notify/Notify/Notify/ViewModels/FriendsPageViewModel.cs
+++ Notify/Notify/Notify/ViewModels/FriendsPageViewModel.cs
@@ -1,25 +1,32 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 
 namespace Notify.ViewModels
 {
     public class FriendsPageViewModel : INotifyPropertyChanged
     {
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        public Command RefreshFriendsCommand { get; set; }
+        public Command ShowFriendRequestsCommand { get; set; }
+
+        public FriendsPageViewModel()
+        {
+            RefreshFriendsCommand = new Command(onRefreshFriendsClicked);
+            ShowFriendRequestsCommand = new Command(onShowFriendRequestsClicked);
+        }
+
+        private void onShowFriendRequestsClicked()
+        {
+            App.Current.MainPage.DisplayAlert("Show Friends Clicked", "Show Friends Clicked", "OK");
+        }
+
+        private void onRefreshFriendsClicked()
+        {
+            App.Current.MainPage.DisplayAlert("Refresh Friends Clicked", "Refresh Friends Clicked", "OK");
+        }
+
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
