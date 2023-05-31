a4e37fbb31e15ab147a59875a9ce62ae3ff46dfe
Lin Moskovitch <lin.moskovitch@sap.com>
2023-05-31 15:59:07 +0300

    working on BT imrpovments

--- Notify/Notify/Notify/Views/BluetoothSettingsPage.xaml.cs
+++ Notify/Notify/Notify/Views/BluetoothSettingsPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Notify.Bluetooth;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,10 +12,25 @@ namespace Notify.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BluetoothSettingsPage : ContentPage
     {
+        private BluetoothManager m_BluetoothManager;
+        
         public BluetoothSettingsPage()
         {
             InitializeComponent();
             BindingContext = new ViewModels.BluetoothSettingsPageViewModel();
+            m_BluetoothManager = new BluetoothManager();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            m_BluetoothManager.startScanningForDevices();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            m_BluetoothManager.stopScanningForDevices();
         }
     }
 }
