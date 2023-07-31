6f01c26b1d0c6ad1c05791831832f3c50bf923e3
Lin Moskovitch <lin.moskovitch@sap.com>
2023-07-31 23:25:47 +0300

    added animation

--- Notify/Notify/Notify/Views/ProfilePage.xaml.cs
+++ Notify/Notify/Notify/Views/ProfilePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Notify.Views
@@ -12,9 +13,46 @@ namespace Notify.Views
             BindingContext = new ViewModels.ProfilePageViewModel();
         }
         
-        private void CarouselView_OnPositionChanged(object sender, PositionChangedEventArgs e)
+        private void onPressed(object sender, EventArgs e)
         {
-           
+            var button = (ImageButton)sender;
+            button.Opacity = 1.0;
+        }
+
+        private void onReleased(object sender, EventArgs e)
+        {
+            var button = (ImageButton)sender;
+            button.Opacity = 0.3;
+        }
+        
+        private void LocationButton_OnPressed(object sender, EventArgs e)
+        {
+            onPressed(sender, e);
+        }
+
+        private void LocationButton_OnReleased(object sender, EventArgs e)
+        {
+            onReleased(sender, e);
+        }
+
+        private void BlueToothButton_OnPressed(object sender, EventArgs e)
+        {
+            onPressed(sender, e);
+        }
+
+        private void BlueToothButton_OnReleased(object sender, EventArgs e)
+        {
+            onReleased(sender, e);
+        }
+
+        private void WifiButton_OnPressed(object sender, EventArgs e)
+        {
+            onPressed(sender, e);
+        }
+
+        private void WifiButton_OnReleased(object sender, EventArgs e)
+        {
+            onReleased(sender, e);
         }
     }
 }
