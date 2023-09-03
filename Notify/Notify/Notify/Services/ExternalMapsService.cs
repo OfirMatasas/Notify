0762174147548df4293f8448f2c08b40c2d656fd
Lin Moskovitch <lin.moskovitch@sap.com>
2023-09-03 21:31:51 +0300

    fixed google maps initialization

--- Notify/Notify/Notify/Services/ExternalMapsService.cs
+++ Notify/Notify/Notify/Services/ExternalMapsService.cs
@@ -1,3 +1,4 @@
+using System;
 
 namespace Notify.Services
 {
@@ -5,9 +6,7 @@ namespace Notify.Services
     {
         protected static ExternalMapsService m_Instance;
         private static readonly LoggerService r_Logger = LoggerService.Instance;
-
         private static readonly object r_Lock = new object();
-        private static readonly LoggerService r_logger = LoggerService.Instance;
 
         public static ExternalMapsService Instance
         {
@@ -19,13 +18,21 @@ namespace Notify.Services
                     {
                         if (m_Instance == null)
                         {
-                            r_logger.LogError("Failed to initialize ExternalMapsService.");
+                            r_Logger.LogError("Failed to initialize ExternalMapsService.");
                         }
                     }
                 }
-
                 return m_Instance;
             }
+        }
+
+        public static void Initialize(ExternalMapsService instance)
+        {
+            if (m_Instance != null)
+            {
+                throw new InvalidOperationException("ExternalMapsService is already initialized.");
+            }
+            m_Instance = instance;
         }
 
         public abstract void OpenExternalMap(string notificationType);
